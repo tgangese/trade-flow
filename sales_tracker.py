f25ec52a9d5afc4247801fd31d3200880ae85f1f
tgangese <gangesefelixt@gmail.com>
2026-09-10 17:24:03 +0100

    Add dynamic sales input

--- sales_tracker.py
+++ sales_tracker.py
@@ -1,45 +1,29 @@
-#product = input("Enter product:")
+sales = []
+add_more = "yes"
+
+while add_more == "yes":
+
+    product = input(" Enter product: ")
+
+    quantity = int(input("Enter quantity: "))
+
+    price = int(input("Enter price: "))
 
 
-#quantity = int(input("Enter quantity:"))
+    sale = {
+        "product": product,
+        "quantity": quantity,
+        "price": price
+    }
 
+    sales.append(sale)
 
-#price = int(input("Enter price"))
-
-
-
-#sale = {
-
-  #  "product": product,
-
-  #  "quantity": quantity,
-
-   # "price": price
-
-#}
-
-
-#total_sale_amount = sale["quantity"] * sale["price"]
-
-#print("Total sale", total_sale_amount)
-#print("product:",sale["product"])
-#print("quantity:",sale["quantity"])
-#print("price:",sale["price"])
-
-sales = [
-    {"product": "Beans", "quantity": 10, "price": 25000},
-    {"product": "Rice", "quantity": 5, "price": 20000},
-    {"product": "Garri", "quantity": 8, "price": 15000}
-]
-
-for sale in sales:
-    total = sale["quantity"] * sale["price"]
-    print(sale["product"]) 
-    print("Total sale",total )
+    add_more = input("Do you want to add another sale? yes/no: ")
 
 total_revenue = 0
+
 for sale in sales:
     total = sale["quantity"] * sale["price"]
     total_revenue = total_revenue + total
 
-print("Total revenue",total_revenue)
+print("Total revenue", total_revenue)
